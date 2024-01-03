9e4a980277bd2a16de0803a9e62296936526c76f
SolitarySheep <144000388+SolitarySheep@users.noreply.github.com>
2024-01-03 20:21:51 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="min-h-screen bg-neutral-100">
-    <nav class="bg-neutral-200 py-8 mb-10 shadow-xl">
+    <nav class="bg-neutral-200 py-8 mb-10 shadow-xl px-20">
       <div
-        class="container mx-auto flex items-center justify-between text-neutral-600"
+        class="container mx-auto flex items-center justify-between text-[#215a57] font-bold"
       >
         <NuxtLink
           to="/"
-          class="text-3xl font-bold text-neutral-500 hidden md:block"
-          >Startup hamsters</NuxtLink
+          class="text-3xl font-bold text-neutral-500 hidden md:flex"
         >
+          <img
+            src="/assets/images/logo.png"
+            alt="Image du logo"
+            width="70px"
+            height="auto"
+          />
+          <p class="pt-10 pl-3 text-[#61b558]">Nature Hamster</p>
+        </NuxtLink>
 
         <div class="lg:hidden">
           <button
@@ -38,37 +45,107 @@
         >
           <NuxtLink
             to="/produit"
-            class="hover:text-neutral-500 block lg:inline-block pt-6 md:pt-0"
-            >Produits</NuxtLink
+            class="hover:text-[#2C7773] block lg:inline-block pt-6 md:pt-0"
+          >
+            <div class="justify-center hidden md:flex">
+              <img
+                src="/assets/images/produit.png"
+                alt="Image du logo"
+                width="40px"
+                height="auto"
+              />
+            </div>
+            Produits</NuxtLink
           >
           <NuxtLink
             to="/hamster"
-            class="hover:text-neutral-500 block lg:inline-block pt-6 md:pt-0"
-            >Hamsters</NuxtLink
+            class="hover:text-[#2C7773] block lg:inline-block pt-6 md:pt-0"
+          >
+            <div class="justify-center hidden md:flex">
+              <img
+                src="/assets/images/hamster.png"
+                alt="Image du logo"
+                width="66px"
+                height="auto"
+              />
+            </div>
+            Hamsters</NuxtLink
           >
           <NuxtLink
             to="/equipe"
-            class="hover:text-neutral-500 block lg:inline-block pt-6 md:pt-0"
-            >Equipe</NuxtLink
+            class="hover:text-[#2C7773] block lg:inline-block pt-6 md:pt-0"
+          >
+            <div class="justify-center hidden md:flex">
+              <img
+                src="/assets/images/equipe.png"
+                alt="Image du logo"
+                width="40px"
+                height="auto"
+              />
+            </div>
+            Equipe</NuxtLink
           >
           <NuxtLink
             to="/blog"
-            class="hover:text-neutral-500 block lg:inline-block pt-6 md:pt-0"
-            >Blog</NuxtLink
+            class="hover:text-[#2C7773] block lg:inline-block pt-6 md:pt-0"
+          >
+            <div class="justify-center hidden md:flex">
+              <img
+                src="/assets/images/blog.png"
+                alt="Image du logo"
+                width="50
+                px"
+                height="auto"
+              />
+            </div>
+            Blog</NuxtLink
           >
           <NuxtLink
             to="/contact"
-            class="hover:text-neutral-500 block lg:inline-block pt-6 md:pt-0"
-            >Contact</NuxtLink
+            class="hover:text-[#2C7773] block lg:inline-block pt-6 md:pt-0"
+          >
+            <div class="justify-center hidden md:flex">
+              <img
+                src="/assets/images/contact.png"
+                alt="Image du logo"
+                width="67px"
+                height="auto"
+              />
+            </div>
+            Contact</NuxtLink
           >
         </div>
       </div>
     </nav>
 
-    <div class="container mx-auto">
+    <div class="container mx-auto px-20">
       <slot />
     </div>
   </div>
+
+  <footer class="bg-neutral-200 pt-6 pb-3 text-neutral-600 text-xl px-20">
+    <div class="container mx-auto md:flex justify-between">
+      <p class="pb-6 md:pb-0">
+        &copy;2023 Nature Hamster. Tous droits réservés.
+      </p>
+      <div class="ml-auto">
+        <a
+          href="#"
+          target="_blank"
+          class="text-neutral-600 hover:text-neutral-500 text-5xl md:text-3xl pr-3"
+        >
+          <i class="ri-instagram-fill"></i>
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          class="text-neutral-600 hover:text-neutral-500 ml-auto text-5xl md:text-3xl"
+        >
+          <i class="ri-facebook-box-fill"></i>
+        </a>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
